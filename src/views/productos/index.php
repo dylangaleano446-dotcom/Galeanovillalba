8a6b3b45af96e35b2fc248a4288d543fcfa997f6
dylangaleano446-dotcom <dylangaleano446@gmail.com>
2026-09-13 20:16:49 -0300

    Update index.php

--- src/views/productos/index.php
+++ src/views/productos/index.php
@@ -2,9 +2,23 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Listado</title>
+    <title>Materiales de Construcción - Productos</title>
 </head>
 <body>
-    <h1>Listado de Entidad</h1>
+    <h1>Listado de Materiales</h1>
+
+    <ul>
+        <?php foreach ($productos as $producto): ?>
+            <li>
+                <strong>ID:</strong> <?= $producto['id'] ?> | 
+                <strong>Producto:</strong> <?= htmlspecialchars($producto['name']) ?> | 
+                <strong>Precio:</strong> $<?= $producto['price'] ?> | 
+                <strong>Descripción:</strong> <?= htmlspecialchars($producto['descripcion']) ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+    <br>
+    <a href="/create/productos">Crear nuevo producto</a>
 </body>
 </html>
