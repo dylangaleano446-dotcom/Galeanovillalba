60d24f2a394d4b4da27e728b31c6b2e650421e99
dylangaleano446-dotcom <dylangaleano446@gmail.com>
2026-09-13 18:51:09 -0300

    Create show.php

---
+++ src/views/productos/show.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Detalle</title>
+</head>
+<body>
+    <h1>Detalle de la Entidad <?= htmlspecialchars($id) ?></h1>
+</body>
+</html>
