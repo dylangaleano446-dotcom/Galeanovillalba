505fceac97d4230c3138d50f677ab15317b2eabe
dylangaleano446-dotcom <dylangaleano446@gmail.com>
2026-09-13 19:15:25 -0300

    Update store.php

--- src/views/productos/store.php
+++ src/views/productos/store.php
@@ -2,9 +2,29 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Crear</title>
+    <title>Crear Producto</title>
 </head>
 <body>
-    <h1>Creando Entidad</h1>
+    <h1>Crear Nuevo Producto</h1>
+
+  
+    <form action="/productos" method="POST">
+        <div>
+            <label for="nombre">Nombre del producto:</label><br>
+            <input type="text" id="nombre" name="nombre" required>
+        </div>
+        <br>
+        <div>
+            <label for="precio">Precio:</label><br>
+            <input type="number" step="0.01" id="precio" name="precio" required>
+        </div>
+        <br>
+        <div>
+            <label for="descripcion">Descripción:</label><br>
+            <textarea id="descripcion" name="descripcion" rows="4" required></textarea>
+        </div>
+        <br>
+        <button type="submit">Guardar Producto</button>
+    </form>
 </body>
 </html>
